c8ec6c53d753a27d5194b38ad18f9866adfb3d6a
kyunghyukcho <rudgur1996@hanmail.net>
2022-08-07 02:20:58 +0900

    update

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,6 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import java.util.List;
 
 public class JpaMain {
@@ -23,7 +20,7 @@ public class JpaMain {
 
             // 회원 수정
 //            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("helloJPA");
+//            findMember.setName("helloJPA"); // persist 가 필요하지 않습니다. < 변경감지 - 플러시 참조 >
 
             // 회원 삭제
 //            em.remove(findMember);
@@ -36,7 +33,40 @@ public class JpaMain {
 //                    .setMaxResults(10) // 객체지향 query 이기 때문에 각 DB의 방언에 맞춰 번역합니다.
 //                    .getResultList();
 
-            tx.commit();
+            //영속
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//            em.persist(member1); // 여기까지 컨텍스트에 쌓인다. 실제 DB에 올라가지 않음
+//            em.persist(member2);
+            //준영속
+//            em.detach(member);
+//            em.clear();
+
+            //플러쉬
+//            Member member = new Member(200, "member200");
+//            em.persist(member);
+//            em.flush();
+
+            //기본키 매핑
+//            Member member = new Member();
+//            member.setId("ID_A");
+//            member.setUsername("C");
+//            em.persist(member);
+
+            //GeneratedValue 매핑
+            // IDENTITY 의 경우.. 기본키를 부여하는것은 데이터베이스에 접근해야 알 수 있다.
+            // 하지만 영속성 컨테스트에 올라가기 때문에 아직 기본키를 알 수 없다.
+            // 이를 해결하기 위해.. IDENTITY 에 한해서 em.persist 시점에 쿼리를 날린다.
+
+            // SEQUENCE 의 경우.. db의 MEMBER_SEQ 에서 부여받을 수 있다
+            // persist 할 경우 db에서 pk값을 가져와 id를 부여하고 영속시킨다
+            // 네트워크 혼잡을 최적화 시키기 위해 미리 50개의 사이즈만 땡겨옵니다! (allocationSize)
+//            Member member = new Member();
+//            member.setUsername("C");
+//            em.persist(member);
+
+
+            tx.commit(); // 실제 데이터베이스에 올리는 CODE
         } catch (Exception e) {
             tx.rollback();
         } finally {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,28 +1,58 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
+//@Table(name="MBR")
+//@Table(uniqueConstraints = )
+@SequenceGenerator(
+        name = "member_seq_generator",
+        sequenceName = "member_seq",
+        initialValue = 1, allocationSize = 50)
+        // 네트워크 혼잡을 최적화 시키기 위해 미리 50개의 사이즈만 땡겨옵니다!
 public class Member {
 
+    //////////////////// 기본 키 매핑 //////////////////////
     @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+    // SEQUENCE 의 경우 String 이 아닌 Long or Integer ..
+    // Table 마다 시퀀스를 따로 관리하고자 한다면 SequenceGenerator 을 사용하면 됩니다.
     private Long id;
-    private String name;
 
-    public Long getId() {
-        return id;
-    }
+    @Column(name = "name", nullable = false)
+    private String username;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public Member() { }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    //////////////////// 매핑 기본 //////////////////////
+//    @Id
+//    private Long id;
+//    //  @Column(unique = true, length = 10)
+//    @Column(name = "name", nullable = false)
+//    private String username;
+//    private Integer age;
+//    @Enumerated(EnumType.STRING) // 디폴트로 쓰지 말것!! -> 디폴트로 사용할 시 순서가 부여됨
+//    private RoleType roleType;
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date createdDate;
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date lastModifiedDate;
+//    private LocalDate testLocalDate;
+//    private LocalDateTime testLocalDateTime;
+//    @Lob
+//    private String description;
+//    @Transient
+//    // 테이블에 올리지 않고 메모리에서만 사용함
+//    private int temp;
+//    public Member() {
+//    }
 }
